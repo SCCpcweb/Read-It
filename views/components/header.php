66de55303ed0804fd78b6db1e9fe278f53a22b01
shookstra <samhookstra@live.com>
2019-11-03 17:15:43 -0600

    Added Site Logo
    Added site logo.

--- views/components/header.php
+++ views/components/header.php
@@ -3,5 +3,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <link rel="stylesheet" href="css/styling.css" />
+    <link rel="icon" href="images/book.png" />
     <title>Read-It</title>
 </head>
